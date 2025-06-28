f3ab86df6c1650b978e9b941a630027d5b3ff003
linked <linkedmachine@gmail.com>
2025-06-28 19:14:27 +0100

    remove client stuff from character.gd

--- character.gd
+++ character.gd
@@ -31,17 +31,11 @@ func _ready() -> void:
 	if( int( char_id.replace("ply_", ""))  == id ):
 		camera.current = is_multiplayer_authority()
 
-var direction := Vector3.ZERO
-var jump: bool = false
-var sprint: bool = false
-
 func _physics_process(delta: float) -> void:
 	
 	if not is_multiplayer_authority():
 		return
-	
-	#camera.rotation.x = pitch
-	
+		
 	if Input.is_action_just_pressed("quit"):
 		$"../".exit_game(name.to_int())
 		get_tree().quit()
@@ -71,52 +65,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 2.0)
-		
-	#viewbob
-	vb_sin += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = headbob(vb_sin)
-	
-	# FOV
-	var velocity_clamped = clamp(velocity.length(), 0.5, SPEED_SPRINT * 2)
-	var target_fov = FOV_BASE + FOV_CHANGE * velocity_clamped
-	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 	
 	#direction = Vector3.ZERO
 	jump = false
-	sprint = false
 
 	move_and_slide()
-	
-func headbob(time) -> Vector3:
-	var pos = Vector3.ZERO
-	pos.y = sin(time * vb_frequency) * vb_amp
-	pos.x = cos(time * vb_frequency / 2) * vb_amp
-	return pos
-	
-# A player using an entity
-func use():
-	var ent = cast_ray()
-
-	if ent != null and ent.has_method("on_use"):
-		rpc_id(1, "sv_use", ent.name)  # 1 = server
-		#queue_free()
-
-func cast_ray():
-	var from = camera.global_position
-	var to = from + camera.global_transform.basis.z * -USE_RANGE  # negative Z is forward
-
-	var space_state = get_world_3d().direct_space_state
-	var query = PhysicsRayQueryParameters3D.create(from, to)
-	query.exclude = [self]
-
-	var result = space_state.intersect_ray(query)
-	if result and result.collider:
-		return result.collider
 
 @rpc("any_peer", "call_remote", "reliable")
 func sv_use(item_name: String):
 	var ply_id: int = multiplayer.get_remote_sender_id()
-	var ply: Node = get_node_or_null("/root/Main/Players/" + str(ply_id))
+	var ply: Node = PlyFuncs.get_ply_from_id(ply_id)
 
 	var item: Node = get_node_or_null("/root/Main/Items/" + str(item_name))
 	if item and is_instance_valid(item) and multiplayer.is_server():
